2aa609c0d514a9070e69e873d39b3e062936d4b7
Surnak <ee521@mail.ru>
2016-03-26 12:03:56 +0600

    Input2

--- lab2.c
+++ lab2.c
@@ -3,6 +3,12 @@
 #include <stdlib.h>
 
 int main(){
-	float a,b,c,d,x1,x2;
+	float a,b,c;
+	printf("Wwedite koeficient a: \n");
+	scanf("%f",&a);
+	printf("Wwedite koeficient b: \n");
+	scanf("%f",&b);
+	printf("Wwedite koeficient c: \n");
+	scanf("%f",&c);
 	return 0;
 }
